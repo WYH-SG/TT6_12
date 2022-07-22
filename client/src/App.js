31177486deef8d9ee63f8e2a2898c1ccc2e9539f
SirPickles <SirPickles@users.noreply.github.com>
2022-07-22 11:38:54 +0800

    added background color and banner
    also bg size changes with window resize

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
+import React, {useState, useEffect} from "react";
 // For React v6 syntax
 // https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
 
@@ -10,10 +10,33 @@ import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 
 function App() {
-  
+  const [windowDimension, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  })
+
+  const DetectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    })
+    useEffect(()=>{
+      window.addEventListener('resize', DetectSize)
+      return()=>{
+        window.removeEventListener('resize', DetectSize)
+      }
+    },[windowDimension])
+  }
   return (
-    <div className="App">
+
+    <div className="App" style={{backgroundColor: "#363636",
+                                 height: windowDimension.winHeight,
+                                 width: windowDimension.winWidth,}}>
       {/* Using React Router to route to pages path */}
+      <h1 style ={{backgroundColor: "white", 
+                   textAlign: "left",
+                   padding: "10px 10px",
+                   }}>DBS app login</h1>
       <Router>
         {/* Create a link to route user to another page */}
         <Link to ="/"> Login Page </Link>
