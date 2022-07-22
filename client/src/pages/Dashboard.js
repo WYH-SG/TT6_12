4d654cdf148574a0453f7d3600abb6e45b3129d8
WYH-SG <70835146+WYH-SG@users.noreply.github.com>
2022-07-22 12:41:27 +0800

    Uppercase Dashboard function
    - debug error on useState() function not working.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, UseEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-function dashboard() {
+function Dashboard() {
+
+  const [displayUsername, setDisplayUsername] = useState("");
+
+  // Test function on a button to check if user is authenticated
+  const userAuthenticated = () => {
+    axios.get("http://localhost:3001/isUserAuth", {
+        // Match ths headers of  "x-access-token" against index.js function verifyJWT
+        // This is to check if the token is matched and return a message of user is authenticated
+        headers: {
+            "x-access-token": localStorage.getItem("token"),
+        }
+    }).then((response) => {
+        console.log("userAuthenticated: ", response);
+        setDisplayUsername(response.data);
+    });
+  };
+
   return (
     <div>
         <h1>dashboard</h1>
     </div>
   )
+
 }
 
-export default dashboard
+export default Dashboard
